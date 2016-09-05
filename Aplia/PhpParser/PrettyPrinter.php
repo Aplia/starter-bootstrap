8d7c26cc8e6b7d9b0610ca7a4233ec2496055528
Jan Borsodi <jb@aplia.no>
2016-09-05 13:39:29 +0200

    Optimized the application creation in cached mode
    The cache mode now contains all required classes in one bundle,
    including the application class itself. This makes it faster loading
    the cached variant as all files are loaded in one go.

---
+++ Aplia/PhpParser/PrettyPrinter.php
@@ -0,0 +1,31 @@
+<?php
+namespace Aplia\PhpParser;
+
+use Aplia\Support\Arr;
+use PhpParser\PrettyPrinter\Standard;
+
+/**
+ * Speciailized pretty printer for PHP code which allows for forcing
+ * the namespace declaration to use the nested form.
+ */
+class PrettyPrinter extends Standard
+{
+    protected $forceNestedNamespace = false;
+
+    public function __construct(array $options = array())
+    {
+        $this->forceNestedNamespace = Arr::get($options, 'forceNestedNamespace', false);
+        parent::__construct($options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function preprocessNodes(array $nodes)
+    {
+        parent::preprocessNodes($nodes);
+        if ($this->forceNestedNamespace) {
+            $this->canUseSemicolonNamespaces = false;
+        }
+    }
+}
