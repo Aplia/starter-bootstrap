13e026ffd8330c059bb9c4f4ec4160f094301eab
Jan Borsodi <jb@aplia.no>
2017-11-01 15:09:45 +0100

    Alias the base_debug_var() function to starter_debug_var()
    This makes all helper function consistent in naming.

--- helpers/debug.php
+++ helpers/debug.php
@@ -12,3 +12,16 @@ if (!function_exists('base_debug_var')) {
         \Aplia\Bootstrap\Base::app()->setDebugVariable($name, $value);
     }
 }
+
+if (!function_exists('starter_debug_var')) {
+    /**
+     * Sets a debug variable to be displayed on the error page when an error occur.
+     * This is useful when debugging errors to see what variables contain.
+     *
+     * Setting the same varible multiple times will simply overwrite the previous value.
+     */
+    function starter_debug_var($name, $value)
+    {
+        \Aplia\Bootstrap\Base::app()->setDebugVariable($name, $value);
+    }
+}
